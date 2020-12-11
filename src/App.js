fa4686c4729cef7d3372dc8a1876e8841672b77d
manvendra-rajpoot <manvendra141986@gmail.com>
2020-12-11 18:58:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   
   
   return (
     <div className="app">
-      <h2>Welcome to My World!</h2>
-      
+      <div className='app__body' >
+        <Sidebar />
+
+        {/* chat */}
+      </div>
     </div>
   );
 }
